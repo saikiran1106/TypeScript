a5d91496dc4b1c13c2ba1a3cd1c50b940a14e9f2
saikiran1106 <135025928+saikiran1106@users.noreply.github.com>
2023-06-13 11:02:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
